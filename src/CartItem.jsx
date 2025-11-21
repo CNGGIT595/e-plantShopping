c5a43c543bb63914aa59110bb7b9173ce2277266
CNGGIT595 <chong.xiao@ato.gov.au>
2025-11-21 08:36:49 -0500

    updated cart quantity count

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -36,7 +36,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     let totalCost = 0;
-    totalCost += parseFloat(item.cost.substring(1)) * item.quantity;
+    totalCost = parseFloat(item.cost.substring(1)) * item.quantity;
     return totalCost;
   };
 
